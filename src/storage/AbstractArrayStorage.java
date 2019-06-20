0fa3dd7b1fe51440d82154c6aceefe5f49f243b9
Andrey1981spb <andrey1981spb@gmail.com>
2019-06-20 23:44:55 +0300

    save, update, delete methods hav been defined
       pattern method is used

---
+++ src/storage/AbstractArrayStorage.java
@@ -0,0 +1,42 @@
+package storage;
+
+import model.Resume;
+
+import java.util.Arrays;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    protected abstract int getIndex(String uuid);
+
+    public abstract void save(Resume resume);
+
+    public abstract void update(Resume resume);
+
+    public abstract void delete(String uudi);
+}
---
+++ src/storage/SortedArrayStorage.java
@@ -0,0 +1,60 @@
+package storage;
+
+import model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+
+
+    @Override
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            Resume searchKey = new Resume();
+            searchKey.setUuid(uuid);
+            int position = Arrays.binarySearch(storage, 0, size, searchKey);
+            storage[index] = null;
+            System.arraycopy(storage,position+1,storage, position, size-position+1);
+            storage[size-1] = null;
+            size--;
+        }
+    }
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + resume.getUuid() + " not exist");
+        } else {
+            Resume searchKey = new Resume();
+            searchKey.getUuid();
+            int position = Arrays.binarySearch(storage, 0, size, searchKey);
+            storage[position] = resume;
+        }
+    }
+
+
+    @Override
+    public void save(Resume resume) {
+        if (getIndex(resume.getUuid()) != -1) {
+            System.out.println("Resume " + resume.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            Resume searchKey = new Resume();
+            searchKey.getUuid();
+            int position = Arrays.binarySearch(storage, 0, size, searchKey);
+            storage[position] = resume;
+            size++;
+        }
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+}
