b2a4ff56ae68cecfab7ed4a3afaac812963532ae
Andrey1981spb <andrey1981spb@gmail.com>
2019-06-29 15:19:49 +0300

    ArrayStorageTest and SortedArrayStorageTest realised with ArrayStorage and SortedArrayStorage, accordingly

---
+++ test/ArrayStorageTest.java
@@ -0,0 +1,12 @@
+import storage.ArrayStorage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    private final ArrayStorage arrayStorage;
+
+    public ArrayStorageTest() {
+        super();
+        arrayStorage = new ArrayStorage();
+    }
+
+}
---
+++ test/SortedArrayStorageTest.java
@@ -0,0 +1,11 @@
+import storage.SortedArrayStorage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    private final SortedArrayStorage sortedArrayStorage;
+
+    public SortedArrayStorageTest() {
+        super();
+        sortedArrayStorage = new SortedArrayStorage();
+    }
+}
