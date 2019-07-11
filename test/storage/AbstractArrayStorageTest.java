4a62b6cabb41cde57785f295816c5c479a056677
Andrey1981spb <andrey1981spb@gmail.com>
2019-07-11 23:46:11 +0300

    tread code duplicate in test-classes

---
+++ test/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,30 @@
+package storage;
+
+import exception.StorageException;
+import model.Resume;
+import org.junit.Assert;
+import org.junit.Test;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
+
+    public Storage storage;
+
+    public AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+        this.storage=storage;
+    }
+
+    @Test(expected = StorageException.class)
+    public void storageOverFlow() {
+        storage.clear();
+        try {
+            for (int i = 0; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail("not overflow yet");
+        }
+        storage.save(new Resume());
+    }
+
+}
