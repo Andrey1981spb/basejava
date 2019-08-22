2b61f9815c2a59566c262aa059b4d576cb7d1251
Andrey1981spb <andrey1981spb@gmail.com>
2019-08-22 11:28:36 +0300

    HW10  add writeWithException-method,  EntryWriter-interface  Change outSerialize-method with lambda

--- src/ru/javawebinar/basejava/storage/serializeUtil/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializeUtil/DataStreamSerializer.java
@@ -5,9 +5,9 @@ import ru.javawebinar.basejava.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class DataStreamSerializer implements Serializer {
 
@@ -17,53 +17,56 @@ public class DataStreamSerializer implements Serializer {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
             Map<ContactType, String> contacts = resume.getContactInfoMap();
-            dos.writeInt(contacts.size());
-            for (Entry<ContactType, String> entry : contacts.entrySet()) {
+            writeWithException(dos, contacts.entrySet(), entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
-            }
+            });
 
-            Map<SectionType, AbstractSection> sections = resume.getResumeSections();
-            dos.writeInt(sections.size());
-            for (Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
-                SectionType sectionType = entry.getKey();
-                AbstractSection section = entry.getValue();
-                dos.writeUTF(sectionType.name());
+            writeWithException(dos, resume.getResumeSections().entrySet(), entry -> {
+                        SectionType type = entry.getKey();
+                        AbstractSection section = entry.getValue();
+                        dos.writeUTF(type.name());
+                        switch (type) {
+                            case PERSONAL:
+                            case OBJECTIVE:
+                                dos.writeUTF(((SimpleTextSection) section).getPosition());
+                                break;
+                            case ACHIEVEMENT:
+                            case QUALIFICATIONS:
+                                writeWithException(dos, ((MarkedListSection) section).getPerformanceList(), dos::writeUTF);
+                                break;
+                            case EXPERIENCE:
+                            case EDUCATION:
+                                writeWithException(dos, ((OrganizationSection) section).getWorkStudyStringDates(), org -> {
+                                    dos.writeUTF(org.getHomePage().getName());
+                                    dos.writeUTF(org.getHomePage().getUrl());
+                                    writeWithException(dos, org.getPositionList(), position -> {
+                                        dos.writeUTF(position.getTitle());
+                                        doWriteLocalDate(dos, position.getDateOfEntry());
+                                        doWriteLocalDate(dos, position.getDateOfExit());
+                                        dos.writeUTF(position.getDescription());
+                                    });
+                                });
+                                break;
+                        }
+                    }
+            );
 
-                switch (sectionType) {
-                    case PERSONAL:
-                    case OBJECTIVE:
-                        dos.writeUTF(((SimpleTextSection) section).getPosition());
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        List<String> performanceList = ((MarkedListSection) section).getPerformanceList();
-                        dos.writeInt(performanceList.size());
-                        for (String performance : performanceList) {
-                            dos.writeUTF(performance);
-                        }
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        List<Organization> organizations = ((OrganizationSection) section).getWorkStudyStringDates();
-                        dos.writeInt(organizations.size());
-                        for (Organization organization : organizations) {
-                            dos.writeUTF(organization.getHomePage().getName());
-                            dos.writeUTF(organization.getHomePage().getUrl());
-                            List<Organization.Position> positionList = organization.getPositionList();
-                            dos.writeInt(positionList.size());
-                            for (Organization.Position position : positionList) {
-                                dos.writeUTF(position.getTitle());
-                                doWriteLocalDate(dos, position.getDateOfEntry());
-                                doWriteLocalDate(dos, position.getDateOfExit());
-                                dos.writeUTF(position.getDescription());
-                            }
-                        }
-                        break;
-                }
-            }
         }
     }
+
+    private <T> void writeWithException(DataOutputStream dos, Collection<T> collection,
+                                       EntryWriter<T> elementWriter) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            elementWriter.writeEntry(item);
+        }
+    }
+
+    private interface EntryWriter<T> {
+        void writeEntry(T t) throws IOException;
+    }
+
 
     @Override
     public Resume inSerialize(InputStream is) throws IOException {
@@ -112,6 +115,7 @@ public class DataStreamSerializer implements Serializer {
         }
         return null;
     }
+
 
     private List<Organization.Position> readPosition(DataInputStream dis) throws IOException {
         int size = dis.readInt();
