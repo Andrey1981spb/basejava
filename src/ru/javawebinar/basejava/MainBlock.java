1e47d0d2518877b390cd7ab8c78ae72664d94c43
Andrey1981spb <andrey1981spb@gmail.com>
2019-08-25 22:12:59 +0300

    Change MainBlock, MainConcurrency-classes

--- src/ru/javawebinar/basejava/MainBlock.java
+++ src/ru/javawebinar/basejava/MainBlock.java
@@ -1,23 +1,31 @@
 package ru.javawebinar.basejava;
 
-public class MainBlock implements Runnable {
+public class MainBlock {
 
-    static ClassA classA = new ClassA();
-    static ClassB classB = new ClassB();
+    final static String lock1 = "lock1";
+    final static String lock2 = "lock2";
 
-    MainBlock() {
-        Thread.currentThread().setName("Главный поток");
-        Thread thread1 = new Thread(this, "Второй поток");
-        thread1.start();
-        classA.processA(classB);
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println("Holding " + lock2);
+                }
+            }
+        }).start();
     }
 
     public static void main(String[] args) {
-        new MainBlock();
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
     }
 
-    @Override
-    public void run() {
-        classB.processB(classA);
-    }
 }
