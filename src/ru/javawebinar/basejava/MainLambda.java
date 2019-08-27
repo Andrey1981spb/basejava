d41eba897a0769c0416079dcf71dc4f79d98dbe4
Andrey1981spb <andrey1981spb@gmail.com>
2019-08-27 22:54:32 +0300

    add MainLambda-class with minValue and oddOrEven-methods

--- src/ru/javawebinar/basejava/MainLambda.java
+++ src/ru/javawebinar/basejava/MainLambda.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.partitioningBy;
 import static java.util.stream.Collectors.toList;
@@ -12,18 +11,18 @@ import static java.util.stream.Collectors.toList;
 public class MainLambda {
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce(0,(a,b)->a*10+b);
+        return Arrays.stream(values).distinct().sorted().reduce(0, (a, b) -> a * 10 + b);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-            Map<Boolean, List<Integer>> map = integers.stream()
-                    .collect(partitioningBy(x -> x % 2 == 0, toList()));
-            return map.get(map.get(false).size() % 2 != 0);
+        Map<Boolean, List<Integer>> map = integers.stream()
+                .collect(partitioningBy(x -> x % 2 == 0, toList()));
+        return map.get(map.get(false).size() % 2 != 0);
 
     }
 
     public static void main(String[] args) {
-        int[] a = {3,3,2,1};
+        int[] a = {3, 3, 2, 1};
         System.out.println(minValue(a));
 
         List<Integer> integers = new ArrayList<>();
